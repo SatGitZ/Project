0c291bc55df29ad20661a47183b20abe9ad1fb69
prakash <srijay0311@gmail.com>
2025-08-18 17:03:16 +0530

    edited test_api.sh

--- test_api.sh
+++ test_api.sh
@@ -13,13 +13,9 @@ for i in {1..15}; do
   echo "Waiting for app... ($i)"
   sleep 2
 done
-NUM1=5
-NUM2=6
-RESULT=$((NUM1 + NUM2))
-EXPECTED=$(curl -s "http://localhost:5000/add?num1=$NUM1&num2=$NUM2")
+EXPECTED="30"
+RESULT=$(curl -s "http://localhost:5000/add?num1=10&num2=20")
 
-echo "NUM1=$NUM1"
-echo "NUM2=$NUM2"
 echo "RESULT=$RESULT"
 echo "EXPECTED=$EXPECTED"
 
